25121c6055af52ff0922b44614cbcda643047aac
Ferdenand S. Bayadas <bayadasf04@gmail.com>
2025-10-30 17:38:12 +0800

    add file and folder for html css folder and image also add javascript

--- onlinedasurvapp/urls.py
+++ onlinedasurvapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.navbar, name='navbar'),
+    path('', views.home, name='home'),
+    path('', views.shop, name='shop'),
+    
 ]
--- onlinedasurvapp/views.py
+++ onlinedasurvapp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'onlinedasurvapp/index.html')
+
+def navbar(request):
+    return render(request, 'onlinedasurvapp/navbar.html')
+
+def home(request):
+    return render(request, 'onlinedasurvapp/home.html')
+
+def shop(request):
+    return render(request, 'onlinedasurvapp/shopitems.html')
