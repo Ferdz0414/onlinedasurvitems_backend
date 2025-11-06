13dccbd0f29a1ee194c8b14af3c6e2a9268a62de
Ferdenand S. Bayadas <bayadasf04@gmail.com>
2025-11-06 10:29:23 +0800

    Prepare for Railway deployment

--- onlinedasurvbackend/settings.py
+++ onlinedasurvbackend/settings.py
@@ -5,13 +5,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/django-insecure-123abcxyz!@#
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7w70%vk@&urne1u4j#61f(ee760^lj^wi76st=kva@5q20m9mb'
+PORT = 8080
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -104,8 +105,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os. path.join(BASE_DIR, 'static')]
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
